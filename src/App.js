fcd4914c2e1b50fc33f2031f24aeeb98957f05e1
altair <74534830+yeabsra-leul@users.noreply.github.com>
2023-03-30 14:22:44 +0300

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Routes,Route, Navigate } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import { About } from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import "./App.scss"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route index path="/" element={<Navigate to="/home" replace/>}/>
+            <Route path="/home" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/skills" element={<Skills/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/services" element={<Services/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
